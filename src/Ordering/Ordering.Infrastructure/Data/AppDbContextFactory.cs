78ab46510a5d0f59d422064610e71a67a358a87e
IT_SUPPCHAI <IT_SUPPCHAI@PROGRAMMER>
2021-03-12 14:19:15 +0700

    Building Ordering Microservices with Clean Architecture and CQRS

---
+++ src/Ordering/Ordering.Infrastructure/Data/AppDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Ordering.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Infrastructure
+{
+    public class AppDbContextFactory: IDesignTimeDbContextFactory<OrderContext>
+    {
+        OrderContext IDesignTimeDbContextFactory<OrderContext>.CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<OrderContext>();
+            optionsBuilder.UseSqlServer("Server=192.168.3.155;Database=OrderDb;User Id=sa;Password=P@ssw0rd;");
+
+            return new OrderContext(optionsBuilder.Options);
+        }
+    }
+}
